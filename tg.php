dc1b454584c6c57470debd99a80e13d8a51515c1
broveymimo <k.arzhanov@alef.im>
2025-04-03 17:39:25 +0300

    wipe

--- tg.php
+++ tg.php
@@ -3,29 +3,4 @@ require_once "requires.php";
 
 use core\Telegram\Telegram;
 
-$json = '{
-  "update_id": 453565858,
-  "message": {
-    "message_id": 521,
-    "from": {
-      "id": 962372340,
-      "is_bot": false,
-      "first_name": "Константин",
-      "last_name": "Аржанов",
-      "username": "Best_profi_admin",
-      "language_code": "ru"
-    },
-    "chat": {
-      "id": 962372340,
-      "first_name": "Константин",
-      "last_name": "Аржанов",
-      "username": "Best_profi_admin",
-      "type": "private"
-    },
-    "date": 1743672638,
-    "text": "боооот"
-  }
-}';
-
-Telegram::getInstance()->mapWebhook($json);
-//Telegram::getInstance()->handleWebhook();
+Telegram::getInstance()->handleWebhook();
